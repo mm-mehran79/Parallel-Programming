97dd01935de5dc876d681eb8eac0b6dc90f3d6b8
mm.mehran79 <mm.mehran79@gmail.com>
2023-02-10 06:41:02 +0330

    fft.cu submitted file added

--- HWs/FFT/files/fft.cu
+++ HWs/FFT/files/fft.cu
@@ -22,6 +22,8 @@
 #define BLOCK_X  1024
 #define THREAD_PER_BLOCK_FLY  256  
 
+//Replace all sincosf with __sincosf for faster speed but lower accuracy (and vice versa)
+
 // you may define other parameters here!
 // you may define other macros here!
 // you may define other functions here!
@@ -85,6 +87,14 @@ __global__ void gpuSort(float* x, float* x_temp, const unsigned int N, const uns
 	x_temp[index2BitReversedOut] = x[index];
 }
 
+__global__ void gpuSortRADIX2(float* x, float* x_temp, const unsigned int N, const unsigned int M)
+{
+	uint index = tx + ( by * BLOCK_X + bx  ) * THREAD_PER_BLOCK_SORT;
+	uint indexBitReversed = __brev(index);
+	uint indexBitReversedOut = indexBitReversed >> (32U-M);
+	x_temp[indexBitReversedOut] = x[index];
+}
+
 __global__ void gpuCopy(float* x, float* x_temp)
 {
 	uint index = tx + ( by * BLOCK_X + bx  ) * THREAD_PER_BLOCK_SORT;
@@ -93,59 +103,6 @@ __global__ void gpuCopy(float* x, float* x_temp)
 
 __global__ void gpuButterfly(float* x_r_d, float* x_i_d, uint v)
 {
-	// uint fl = 1<<v;
-	// int id = tx + ( by * BLOCK_X + bx  ) * THREAD_PER_BLOCK_FLY / 2;
-    // unsigned int sh = id  % fl;
-	// id = (id/fl) * (4*fl)  + sh;
-
-	// float wr = x_r_d[id];
-	// float wi = x_i_d[id];
-	// float arg;
-
-	// id=id+fl;
-	// float x_r_1 = x_r_d[id];
-	// float x_i_1 = x_i_d[id];
-	// arg = (PI * sh )/(2*fl);
-	// float cos_r, sin_r;
-    // // float cos_r = cos(arg);
-	// // float sin_r = sin(arg);
-	// __sincosf(arg, &sin_r, &cos_r);
-	// float wr_1 = cos_r*x_r_1+sin_r*x_i_1;
-	// float wi_1 = -sin_r*x_r_1+cos_r*x_i_1;
-
-	// arg=arg*2;
-    // // cos_r = cos(arg);
-    // // sin_r = sin(arg);
-	// __sincosf(arg, &sin_r, &cos_r);
-	// id=id+fl;
-	// x_r_1 = x_r_d[id];
-	// x_i_1 = x_i_d[id];
-	// float wr_2 = cos_r*x_r_1+sin_r*x_i_1;
-	// float wi_2 = -sin_r*x_r_1+cos_r*x_i_1;
-
-	// arg=arg*3.0/2.0;
-    // // cos_r = cos(arg);
-    // // sin_r = sin(arg);
-	// __sincosf(arg, &sin_r, &cos_r);
-	// id=id+fl;
-	// x_r_1 = x_r_d[id];
-	// x_i_1 = x_i_d[id];
-	// float wr_3 = cos_r*x_r_1+sin_r*x_i_1;
-	// float wi_3 = -sin_r*x_r_1+cos_r*x_i_1;
-
-	// x_r_d[id] = wr - wi_1 - wr_2 + wi_3;
-	// x_i_d[id] = wi + wr_1 - wi_2 - wr_3;
-	// id=id-fl;
-	// x_r_d[id] = wr - wr_1 + wr_2 - wr_3;
-	// x_i_d[id] = wi - wi_1 + wi_2 - wi_3;
-	// id=id-fl;
-	// x_r_d[id]     = wr + wi_1 - wr_2 - wi_3;
-	// x_i_d[id]     = wi - wr_1 - wi_2 + wr_3;
-	// id=id-fl;
-	// x_r_d[id]          = wr + wr_1 + wr_2 + wr_3;
-	// x_i_d[id]          = wi + wi_1 + wi_2 + wi_3;
-	///////////////////////////////////////////////////// 2nd way:
-
 	uint index = tx + ( by * BLOCK_X + bx  ) * THREAD_PER_BLOCK_FLY / 2;
 	uint fl = 1<<v;
 	uint p = index % fl;
@@ -163,7 +120,7 @@ __global__ void gpuButterfly(float* x_r_d, float* x_i_d, uint v)
 	indexNew += fl;
 	x_r_temp = x_r_d[indexNew];
 	x_i_temp = x_i_d[indexNew];
-	sincosf(theta, &sinus, &cosinus);
+	__sincosf(theta, &sinus, &cosinus);
 	w1_real = cosinus * x_r_temp + sinus * x_i_temp;
 	w1_imag = cosinus * x_i_temp - sinus * x_r_temp;
 
@@ -171,7 +128,7 @@ __global__ void gpuButterfly(float* x_r_d, float* x_i_d, uint v)
 	float theta_temp = theta + theta;
 	x_r_temp = x_r_d[indexNew];
 	x_i_temp = x_i_d[indexNew];
-	sincosf(theta_temp, &sinus, &cosinus);
+	__sincosf(theta_temp, &sinus, &cosinus);
 	w2_real = cosinus * x_r_temp + sinus * x_i_temp;
 	w2_imag = cosinus * x_i_temp - sinus * x_r_temp;
 
@@ -179,7 +136,7 @@ __global__ void gpuButterfly(float* x_r_d, float* x_i_d, uint v)
 	theta_temp += theta;
 	x_r_temp = x_r_d[indexNew];
 	x_i_temp = x_i_d[indexNew];
-	sincosf(theta_temp, &sinus, &cosinus);
+	__sincosf(theta_temp, &sinus, &cosinus);
 	w3_real = cosinus * x_r_temp + sinus * x_i_temp;
 	w3_imag = cosinus * x_i_temp - sinus * x_r_temp;
 
@@ -196,26 +153,79 @@ __global__ void gpuButterfly(float* x_r_d, float* x_i_d, uint v)
 	x_i_d[index] = w0_imag + w1_real - w2_imag - w3_real;
 }
 
+__global__ void gpuButterflyRADIX2(float* x_r_d, float* x_i_d, uint v)
+{
+	uint index = tx + ( by * BLOCK_X + bx  ) * THREAD_PER_BLOCK_FLY;
+	uint fl = 1<<v;
+	uint p = index % fl;
+	uint indexNew = (index/fl)*(2*fl) + p;
+	index = indexNew;
+	float const theta = (PI * p)/(fl);
+	float cosinus, sinus;
+	float x_i_temp, x_r_temp;
+
+
+	float w0_real, w0_imag, w1_real, w1_imag;
+	w0_real = x_r_d[indexNew];
+	w0_imag = x_i_d[indexNew];
+	
+	indexNew += fl;
+	x_r_temp = x_r_d[indexNew];
+	x_i_temp = x_i_d[indexNew];
+	__sincosf(theta, &sinus, &cosinus);
+	w1_real = cosinus * x_r_temp + sinus * x_i_temp;
+	w1_imag = cosinus * x_i_temp - sinus * x_r_temp;
+
+	x_r_d[index] = w0_real + w1_real;
+	x_i_d[index] = w0_imag + w1_imag;
+	index += fl;
+	x_r_d[index] = w0_real - w1_real;
+	x_i_d[index] = w0_imag - w1_imag;
+}
+
 //-----------------------------------------------------------------------------
 void gpuKernel(float* x_r_d, float* x_i_d, const unsigned int N, const unsigned int M)
 {
 	float *d_temp;
 	HANDLE_ERROR(cudaMalloc((void**) & d_temp, N * sizeof(float)));
+	if (M%2)
+	{
+		//radix 2
+		dim3 dimGrid_Swap(BLOCK_X,N/BLOCK_X/THREAD_PER_BLOCK_SORT); 
+		dim3 dimBlock_Swap(THREAD_PER_BLOCK_SORT,1);
+		gpuSortRADIX2<<< dimGrid_Swap,dimBlock_Swap >>>(x_r_d, d_temp, N, M);
+		gpuCopy<<< dimGrid_Swap,dimBlock_Swap >>>(x_r_d, d_temp);
 
-	dim3 dimGrid_Swap(BLOCK_X,N/BLOCK_X/THREAD_PER_BLOCK_SORT); 
-	dim3 dimBlock_Swap(THREAD_PER_BLOCK_SORT,1);
-	gpuSort<<< dimGrid_Swap,dimBlock_Swap >>>(x_r_d, d_temp, N, M);
-	gpuCopy<<< dimGrid_Swap,dimBlock_Swap >>>(x_r_d, d_temp);
+		dim3 dimGrid_Butterfly(BLOCK_X,N/2/BLOCK_X/(THREAD_PER_BLOCK_FLY)); 
+		dim3 dimBlock_Butterfly(THREAD_PER_BLOCK_FLY,1);
+		gpuSortRADIX2<<< dimGrid_Swap,dimBlock_Swap >>>(x_i_d, d_temp, N, M);
+		gpuCopy<<< dimGrid_Swap,dimBlock_Swap >>>(x_i_d, d_temp);
+
+		for(uint i = 0; i < M; i += 1)
+			gpuButterflyRADIX2<<< dimGrid_Butterfly ,dimBlock_Butterfly >>>(x_r_d,x_i_d,i);
+		HANDLE_ERROR(cudaFree(d_temp));
+	}
+	else
+	{
+		//radix 4
+		dim3 dimGrid_Swap(BLOCK_X,N/BLOCK_X/THREAD_PER_BLOCK_SORT); 
+		dim3 dimBlock_Swap(THREAD_PER_BLOCK_SORT,1);
+		gpuSort<<< dimGrid_Swap,dimBlock_Swap >>>(x_r_d, d_temp, N, M);
+		gpuCopy<<< dimGrid_Swap,dimBlock_Swap >>>(x_r_d, d_temp);
+		
+		dim3 dimGrid_Butterfly(BLOCK_X,N/4/BLOCK_X/(THREAD_PER_BLOCK_FLY/2)); 
+		dim3 dimBlock_Butterfly(THREAD_PER_BLOCK_FLY/2,1);
+		gpuSort<<< dimGrid_Swap,dimBlock_Swap >>>(x_i_d, d_temp, N, M);
+		gpuCopy<<< dimGrid_Swap,dimBlock_Swap >>>(x_i_d, d_temp);
+		
+		
+		for(uint i = 0; i < M; i += 2)
+			gpuButterfly<<< dimGrid_Butterfly ,dimBlock_Butterfly >>>(x_r_d,x_i_d,i);
+		HANDLE_ERROR(cudaFree(d_temp));
+	}
 	
-	dim3 dimGrid_Butterfly(BLOCK_X,N/4/BLOCK_X/(THREAD_PER_BLOCK_FLY/2)); 
-	dim3 dimBlock_Butterfly(THREAD_PER_BLOCK_FLY/2,1);
-	gpuSort<<< dimGrid_Swap,dimBlock_Swap >>>(x_i_d, d_temp, N, M);
-	gpuCopy<<< dimGrid_Swap,dimBlock_Swap >>>(x_i_d, d_temp);
-	
-	// HANDLE_ERROR(cudaFree(d_temp));
-	for(uint i = 0; i < M; i += 2)
-		gpuButterfly<<< dimGrid_Butterfly ,dimBlock_Butterfly >>>(x_r_d,x_i_d,i);
-	HANDLE_ERROR(cudaFree(d_temp));
+
+		
 	// In this function, both inputs and outputs are on GPU.
 	// No need for cudaMalloc, cudaMemcpy or cudaFree.
 	// This function does not run on GPU. 
